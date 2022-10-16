6125bdd6b1ece976d670985a8304dbd6bb5d55e0
Kazim Raffiq-Fazal <KR721@student.aru.ac.uk>
2022-10-16 16:06:40 +0100

    Created class files of vehicle and petrol pump, created constructors for these classes but have not created properties for them yet

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Timers;
+
+
+namespace Broken_Petrol_Ltd_2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+        
+
+
+        }
+    }
+
+}
