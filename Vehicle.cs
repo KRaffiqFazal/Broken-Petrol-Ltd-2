2492b732dbd87c8e730ed04dac474f79f8a69529
Kazim Raffiq-Fazal <KR721@student.aru.ac.uk>
2022-10-16 17:35:50 +0100

    Declared variables for the vehicle class and will overwrite appropriately within the inherited specific vehicle class constructors (the type will always be "Car" in the Car class which is inherited from the Vehicle class.

--- Vehicle.cs
+++ Vehicle.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace Broken_Petrol_Ltd_2
 {
     internal class Vehicle
     {
-        
+        public String type;
+        public String fuelType;
+        public int maxFuelCapacity;
+        public int fuelInTank;
+        public System.Timers.Timer fuellingTime;
+        public int fuellingTimerInt;
+        public System.Timers.Timer waitingTime;
         public Vehicle()
         { 
-        
+            
         }
     }
     internal class Car : Vehicle
