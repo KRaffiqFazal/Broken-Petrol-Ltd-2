6007797d48cc6a094ddb8ddef9786106bda7e72c
Kazim Raffiq-Fazal <KR721@student.aru.ac.uk>
2022-10-20 20:32:31 +0100

    Created an async function that will continue assigning vehicles to pumps by checking all pumps that currently are here, also created the pump objects in a 3 dimensional array to simulate the 3 lanes that each have 3 pumps.

--- Program.cs
+++ Program.cs
@@ -7,12 +7,24 @@ namespace Broken_Petrol_Ltd_2
     {
         public static int VehicleCounter = 0;
         public static ArrayList existingVehicles = new ArrayList();
+        public static Pump[,,] pumps = new Pump[3, 3, 3] { { new Pump(1), new Pump(2), new Pump(3) }, { new Pump(4), new Pump(5), new Pump(6)}, {new Pump(7), new Pump(8), new Pump(9) } }
         
         static void Main(string[] args)
         {
 
             Random rnd = new Random();
             Timer carAdder = new Timer(CarAdder, null, 0, rnd.Next(1500, 2200));
+        }
+
+        public static async Task Assigner() //an asynchronous method that will run throughout the entire program to ensure that vehicles are assigned as they come in
+        {
+            await Task.Run(() => 
+            {
+                while (true)
+                { 
+                    //assign vehicles to an available pump
+                }
+            });
         }
 
         public static void CarAdder(object o)
