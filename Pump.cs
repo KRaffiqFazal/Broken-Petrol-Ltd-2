ea5f91a982e8043d17fc45997b622a4b2d530638
Kazim Raffiq-Fazal <KR721@student.aru.ac.uk>
2022-10-20 14:05:32 +0100

    Added a pump class that will create a pump if parsed in the pump's value.

--- Pump.cs
+++ Pump.cs
@@ -6,11 +6,25 @@ using System.Threading.Tasks;
 
 namespace Broken_Petrol_Ltd_2
 {
-    internal class Pump
-    {
-        public Pump()
-        { 
-        
+        internal class Pump //each pump will be an object of this class
+        {
+
+            public int pumpNumber;
+            public double lpgDispensed;
+            public double dieselDispensed;
+            public double unleadedDispensed;
+            public bool inUse;
+
+            public Pump(int pumpNum)
+            {
+                pumpNumber = pumpNum;
+                lpgDispensed = 0;
+                dieselDispensed = 0;
+                unleadedDispensed = 0;
+                inUse = false;
+
+            }
+
+
         }
-    }
 }
