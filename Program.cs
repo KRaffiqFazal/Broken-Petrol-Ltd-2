5cbe593587249843f551fff0fa44084e7fb049fc
Kazim Raffiq-Fazal <KR721@student.aru.ac.uk>
2022-10-21 08:40:36 +0100

    Changed variable in vehicle class to a bool called is fuelling and made it so that it triggers once fuelling starts not ends, also added 3 async methods that will either assign cars to pumps, kick cars that have finished waiting or kick cars that have started fuelling

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace Broken_Petrol_Ltd_2
     class Program
     {
         public static int VehicleCounter = 0;
+        public static bool cont = true; //will signal async functions to stop when it becomes false.
         public static List<Vehicle> existingVehicles = new List<Vehicle>();
         public static Pump[] lane1 = { new Pump(1), new Pump(2), new Pump(3) };
         public static Pump[] lane2 = { new Pump(4), new Pump(5), new Pump(6) };
@@ -16,23 +17,59 @@ namespace Broken_Petrol_Ltd_2
 
             Random rnd = new Random();
             Timer carAdder = new Timer(CarAdder, null, 0, rnd.Next(1500, 2200));
+            Assigner();
+            WaitKick();
+            FuelledKick();
         }
 
         public static async Task Assigner() //an asynchronous method that will run throughout the entire program to ensure that vehicles are assigned as they come in
         {
             await Task.Run(() => 
             {
-                while (true)
+                while (cont)
                 {
                     //assign vehicles to an available pump
-                    foreach (Object o in existingVehicles)
+                    foreach (Vehicle item in existingVehicles)
                     { 
-                        o
+                        
                     }
                 }
             });
         }
-
+        public static async Task WaitKick() //an asynchronous method that will run throughout the entire program to ensure that vehicles are kicked from the list if they finish waiting
+        {
+            await Task.Run(() =>
+            {
+                while (cont)
+                {
+                    //checks list of vehicles to see which want to leave
+                    foreach (Vehicle item in existingVehicles)
+                    {
+                        if (Vehicle.hasWaited == true)
+                        {
+                            existingVehicles.Remove(item);
+                        }
+                    }
+                }
+            });
+        }
+        public static async Task FuelledKick() //an asynchronous method that will run throughout the entire program to ensure that vehicles are kicked from the list once they start fuelling.
+        {    
+            await Task.Run(() =>
+            {
+                while (cont)
+                {
+                    //checks list of vehicles to see which have started fuelling and so can be kicked
+                    foreach (Vehicle item in existingVehicles)
+                    {
+                        if (Vehicle.isFuelling == true)
+                        {
+                            existingVehicles.Remove(item);
+                        }
+                    }
+                }
+            });
+        }
         public static void CarAdder(object o)
         { 
             existingVehicles.Add(new Vehicle(VehicleCounter));
--- Vehicle.cs
+++ Vehicle.cs
@@ -13,16 +13,16 @@ namespace Broken_Petrol_Ltd_2
         public static Timer waitingTime;
         public static int vehicleID;
         public static bool hasWaited;
-        public static bool hasFuelled;
+        public static bool isFuelling;
         public Vehicle(int value)
         {
             Random rnd = new Random();
             String[] model = { "Car", "Van", "HGV" };
             int[] maxFuel = { 50, 80, 150 };
             waitingTime = new Timer(WaitingTime_Elapsed, null, 0, rnd.Next(1000, 2000));
-            vehicleID = value;
+            vehicleID = value; //a unique id that can be used to find a specific vehicle.
             hasWaited = false;
-            fuellingTime = new Timer(FuellingTime_Elapsed, null, Timeout.Infinite, fuellingTimeInt);
+            fuellingTime = new Timer(FuellingTime_Elapsed, null, Timeout.Infinite, fuellingTimeInt); //once specified will trigger to signal the time it takes for a vehicle to fuel
             int temp = rnd.Next(0, 3); //used to select the vehicle type and hence its max fuel capacity.
             type = model[temp];
             maxFuelCapacity = maxFuel[temp];
@@ -39,14 +39,14 @@ namespace Broken_Petrol_Ltd_2
             fuellingTime.Dispose();
         }
         void FuellingTime_Elapsed(object o)
-        {
-            hasFuelled = true;
+        {    
             fuellingTime.Dispose();
 
         }
         void StartingFuelling()
         {
             fuellingTime.Change(0, fuellingTimeInt);
+            isFuelling = true;
             waitingTime.Dispose();
         }
 
